62bea3de966805d7e8edaa6303cc37d80a9cbd95
djswypes@gmail.com <83518572+djswypes@users.noreply.github.com>
2022-07-27 22:31:39 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from morse_code_converter import MorseConverter as morse
+
+code_is_running = True
+
+while code_is_running:
+    user_options = input('Choose from "encrypt" or "decrypt": ')
+
+    if user_options == 'encrypt':
+        message_to_encrypt = input('Type the message you wish to encrypt: ')
+        encrypted_message = morse().encrypt(message_to_encrypt)
+        print(encrypted_message)
+
+    elif user_options == 'decrypt':
+        message_to_decrypt = input('Morse code to decrypt:').strip()
+        decrypted_message = morse().decrypt(message_to_decrypt)
+        print(decrypted_message)
+    else:
+        print('Wrong choice of function!')
+        continue
+
+    to_continue = input('Do you wish to cut the connection "Y"/ "N"? ').title()
+    
+    if to_continue == 'Y':
+        break
+
+    if to_continue == 'N':
+        pass
+
+    else:
+        break
---
+++ morse_code_converter.py
@@ -0,0 +1,72 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+class MorseConverter():
+
+    def __init__(self) -> None:
+        self.cipher = ''
+        self.decipher = ''
+
+    def encrypt(self, message):
+
+        for letter in message.upper():
+            
+            if letter == ' ':
+                self.cipher += ' '
+
+            if letter in MORSE_CODE_DICT:
+                self.cipher += MORSE_CODE_DICT[letter] + ' '
+
+        return self.cipher
+
+    def decrypt(self, morse_code):
+    # Ask for the word
+        morse_code += " "
+        citext = ''
+
+        #check for space
+        for code in morse_code:
+
+            # checks for space
+            if code != ' ':
+
+                citext += code
+
+                i = 0
+            
+            else:
+
+                i += 1
+
+                if i == 2:
+
+                    self.decipher += ' '
+
+                else:
+
+                    try:
+
+                        self.decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(citext)]
+                    
+                    except:
+
+                        pass
+
+                    citext = ''
+
+        return self.decipher
+
+
